27ca1a7c5acfe558e03413d885a6a047309bc04f
Felipe Costa <costa.felipe52@gmail.com>
2020-02-16 00:10:25 -0300

    Curso Web Moderno Udemy

---
+++ fundamentos/comentario.js
@@ -0,0 +1,7 @@
+// comentario de uma linha 
+console.log("Linha 1")
+
+/*
+comentarios de multiplas linhas
+*/
+console.log("Linha 2")
---
+++ fundamentos/usandoVar2.js
@@ -0,0 +1,7 @@
+// evitar variaveis globais 
+var numero = 1
+{
+  var numero = 2
+  console.log('dento =', numero)
+}
+console.log('fora =', numero)
