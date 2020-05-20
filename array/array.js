7a3a0a81641d7303f1de00b524ea35b8b1232e18
Felipe Costa <costa.felipe52@gmail.com>
2020-05-20 19:18:21 -0300

    Curso Web Moderno Udemy

---
+++ array/array.js
@@ -0,0 +1,35 @@
+// array em JS é um objeto
+console.log(typeof Array, typeof new Array, typeof [])
+
+let aprovados = new Array('Bia', 'Carlos', 'Ana')
+console.log(aprovados)
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+
+console.log(aprovados[3])
+
+aprovados[3] = 'Paulo' // mais usado para substituição
+console.log(aprovados[3])
+
+aprovados.push('Abia') // mais usado para adicionar algo
+
+console.log(aprovados.length)
+
+aprovados[9] = 'Rafael'
+console.log(aprovados.length)
+console.log(aprovados[8] === undefined)
+
+console.log(aprovados)
+aprovados.sort() // organiza o arry em ordem crescente
+console.log(aprovados)
+
+delete aprovados[1]
+console.log(aprovados[1])
+console.log(aprovados[2])
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+aprovados.splice(1, 1)
+console.log(aprovados)
