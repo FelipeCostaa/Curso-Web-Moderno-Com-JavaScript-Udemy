26e720bc7d0933bac58ea815a5b09f127b8144f5
Felipe Costa <costa.felipe52@gmail.com>
2020-05-24 15:03:34 -0300

    Curso Web Moderno Udemy

---
+++ node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,13 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola)
+// console.log(moduloA.bemVindo)
+
+// const c = require('./pastaC/index')
+const c = require('./pastaC')
+console.log(c.ola2)
+
+// const http = require('http')
+// http.createServer((req, res) => {
+//   res.write('Bom dia!')
+//   res.end()
+// }).listen(8080)
