95e2fcdf67a47b538e163b93ca7b378a460a9737
Felipe Costa <costa.felipe52@gmail.com>
2020-05-24 00:37:52 -0300

    Curso Web Moderno Udemy

---
+++ node/moduloA.js
@@ -0,0 +1,5 @@
+// tudo que se escreve dentro de um modulo, fica dentro do modulo
+// para tornar disponivel para outro arquivo é necessario importar/ exportar
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao Node!'
+module.exports.ateLogo = 'Até o proximo exemplo'
---
+++ node/moduloB.js
@@ -0,0 +1,6 @@
+module.exports = { // maneira mais comum de exportar modulos
+  bomDia: 'Bom Dia',
+  boaNoite() {
+    return 'Boa Noite'
+  }
+}
